d8e5980cd090d2303020f9d327bb3f510ca65066
robert <robertrenbysanjuan@gmail.com>
2025-03-08 19:37:48 +0800

    update

--- javascript-amazon-project/scripts/amazon.js
+++ javascript-amazon-project/scripts/amazon.js
@@ -47,7 +47,7 @@ products.forEach(product =>{
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${product.name}">
             Add to Cart
           </button>
         </div>
@@ -56,6 +56,26 @@ products.forEach(product =>{
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) =>{
     button.addEventListener('click', ()=>{
-        console.log("add pro");
+        const productName = button.dataset.productName;
+
+        let matchingItem;
+
+        cart.forEach(item => {
+          if(productName === item.productName){
+            matchingItem = item
+          }
+        });
+
+        if(matchingItem){
+          matchingItem.quantity += 1;
+        }else{
+          cart.push({
+            productName: productName,
+            quantity: 1
+          })
+        }
+
+        console.log(cart);
+        
     })
 })
