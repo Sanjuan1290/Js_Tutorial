d2abeb67007eed86eb7fe53c138c7805e9e977ee
robert <robertrenbysanjuan@gmail.com>
2025-03-17 18:28:42 +0800

    create isWeekEnd function and used MVC on CheckOut Items instead of removing it directly

--- javascript-amazon-project/data/deliveryOptions.js
+++ javascript-amazon-project/data/deliveryOptions.js
@@ -25,3 +25,20 @@ export function getDeliveryOption(deliveryOptionId){
 
     return deliveryOption || deliveryOption[0];
 }
+
+export function isWeekEnd(deliveryDate){
+    let dateString = '';
+    if(deliveryDate.format('dddd') === 'Saturday' || deliveryDate.format('dddd') === 'Sunday'){
+        if(deliveryDate.format('dddd') === 'Saturday'){
+            let isSaturday = deliveryDate.add(2, 'days');
+            dateString = isSaturday.format('dddd, MMMM D')
+        }else{
+            let isSunday = deliveryDate.add(1, 'days');
+            dateString = isSunday.format('dddd, MMMM D')
+        }
+    }else{
+        dateString = deliveryDate.format('dddd, MMMM D')
+    }
+
+    return dateString;
+}
--- javascript-amazon-project/scripts/checkout/orderSummary.js
+++ javascript-amazon-project/scripts/checkout/orderSummary.js
@@ -2,7 +2,7 @@ import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeli
 import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
+import {deliveryOptions, getDeliveryOption, isWeekEnd} from '../../data/deliveryOptions.js'
 import { renderPaymentSummary } from './paymentSummary.js'
 
 export function renderOrderSummary(){
@@ -71,13 +71,13 @@ export function renderOrderSummary(){
 
     });
 
-
     function deliveryOptionsHTML(matchingProduct, cartItem) {
         let html = '';
         deliveryOptions.forEach((deliveryOption) => {
             const today = dayjs();
             const deliveryDate = today.add(deliveryOption.deliveryDays, 'days')
-            const dateString = deliveryDate.format('dddd, MMMM D')
+
+            let dateString = isWeekEnd(deliveryDate);
 
             const priceString = deliveryOption.priceCents === 0 ? 'FREE' : `$${formatCurrency(deliveryOption.priceCents)} -`;
 
@@ -111,10 +111,7 @@ export function renderOrderSummary(){
                 const productId = link.dataset.productId
                 removeFromCart(productId);
                 
-                const container = document.querySelector(`.js-cart-item-container-${productId}`)
-
-                container.remove()
-
+                renderOrderSummary();
                 updateCheckOutQuantity();
                 renderPaymentSummary();
                 
