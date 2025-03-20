36ad676d68b74170b0eb177324a66f838b5176fe
robert <robertrenbysanjuan@gmail.com>
2025-03-20 14:06:26 +0800

    skipped testing

--- javascript-amazon-project/scripts/checkout/orderSummary.js
+++ javascript-amazon-project/scripts/checkout/orderSummary.js
@@ -1,7 +1,8 @@
 import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from "../../data/cart.js";
 import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
-import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+
+
 import {deliveryOptions, getDeliveryOption, isWeekEnd} from '../../data/deliveryOptions.js'
 import { renderPaymentSummary } from './paymentSummary.js'
 
@@ -19,7 +20,6 @@ export function renderOrderSummary(){
         const today = dayjs();
         const deliveryDate = today.add(deliveryOption.deliveryDays, 'days')
         const dateString = deliveryDate.format('dddd, MMMM D')
-
         cartSummaryHTML += `
         <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
