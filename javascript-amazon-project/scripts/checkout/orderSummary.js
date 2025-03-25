7f50e2d54e7cf83a17853d5255807ba7af930c0b
robert <robertrenbysanjuan@gmail.com>
2025-03-25 10:37:52 +0800

    load products from the backend

--- javascript-amazon-project/scripts/checkout/orderSummary.js
+++ javascript-amazon-project/scripts/checkout/orderSummary.js
@@ -1,10 +1,11 @@
 import { cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from "../../data/cart.js";
-import { products, getProduct } from "../../data/products.js";
+import { products, getProduct, loadProducts } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
-
 
 import {deliveryOptions, getDeliveryOption, isWeekEnd} from '../../data/deliveryOptions.js'
 import { renderPaymentSummary } from './paymentSummary.js'
+
+
 
 export function renderOrderSummary(){
     let cartSummaryHTML = '';
@@ -167,4 +168,3 @@ export function renderOrderSummary(){
     })
 }
 
-renderOrderSummary();
